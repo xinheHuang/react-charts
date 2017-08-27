7300aa2359a69a8b3318b88b477b4042a538c98f
xinhe huang <huntriver@hotmail.com>
2017-08-27 13:46:45 +0800

    add init demo

---
+++ src/demo/real-time-line-chart-demo/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import LiquidFillGauge from '../../componenets/liquid-fill-gauge';
+
+class RealTimeLineChartDemo extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <p>Real-Time-Line-Chart</p>
+      </div>
+    );
+  }
+}
+
+export default RealTimeLineChartDemo;
